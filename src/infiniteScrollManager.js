f4e510c93b66ee6cbbff626b02776073b8cac9c5
Alejandro de Arriba <adearriba@outlook.com>
2022-11-12 11:13:40 +0100

    Added HTML history push

--- src/infiniteScrollManager.js
+++ src/infiniteScrollManager.js
@@ -4,11 +4,17 @@ export class InfiniteScrollManager {
         const defaults = {
             container: '#scrollContainer',
             pagination: '#scrollPagination',
-            threshold: 0
+            threshold: 0,
+            enableHtml5History: true,
+            pageQueryParamName: 'page',
         };
 
         // Merge custom configs with defaults
         this.settings = Object.assign(defaults, settings);
+        this.currentPage = 1;
+
+        const searchParams = new URL(window.location).searchParams;
+        this.currentPage = searchParams.has(this.settings.pageQueryParamName) ? searchParams.get(this.settings.pageQueryParamName) : this.currentPage;
 
         this.containerElement = document.querySelector(this.settings.container);
         this.paginationElement = document.querySelector(this.settings.pagination);
@@ -42,7 +48,6 @@ export class InfiniteScrollManager {
     }
 
     loadMore() {
-        console.log('Loading...');
         const nextPageLinkElement = this.paginationElement.querySelector('a');
 
         if (!nextPageLinkElement) {
@@ -58,7 +63,8 @@ export class InfiniteScrollManager {
 
     onHttpRequestLoad(event) {
         const request = event.srcElement;
-
+        this.currentPage++;
+        
         if (!request.responseXML) { return; }
         if (!request.readyState === 4 || !request.status === 200) { return; }
 
@@ -66,6 +72,13 @@ export class InfiniteScrollManager {
         const newPagination = request.responseXML.querySelector(this.settings.pagination);
 
         this.containerElement.insertAdjacentHTML('beforeend', newContainer.innerHTML);
+
+        if(this.settings.enableHtml5History) {
+            const url = this.getURLInformation();
+            window.history.pushState({ 
+                page: url.searchParams.get(this.settings.pageQueryParamName)
+            }, '', url);
+        }
 
         if (!newPagination) {
             this.destroy();
@@ -78,6 +91,13 @@ export class InfiniteScrollManager {
                 },
             }));
         }
+    }
+
+    getURLInformation() {
+        const url = new URL(window.location);
+        url.searchParams.set(this.settings.pageQueryParamName, this.currentPage);
+
+        return url;
     }
 }
 
